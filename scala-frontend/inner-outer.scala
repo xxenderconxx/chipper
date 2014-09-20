e96909d989568c1087b39ae9bf8b5bf099752e9b
Howard Mao <zhehao.mao@gmail.com>
2014-09-19 20:38:44 -0700

    get rid of Data

--- scala-frontend/inner-outer.scala
+++ scala-frontend/inner-outer.scala
@@ -2,18 +2,18 @@ import Direction._
 import Builder._
 
 class Inner extends Module {
-  val io = Data(new Bundle {
+  val io = new Bundle {
     val in = Bits(INPUT, 8)
     val out = Bits(OUTPUT, 8)
-  })
+  }
   io.out := io.in + Bits(1)
 }
 
 class Outer extends Module {
-  val io = Data(new Bundle { 
+  val io = new Bundle { 
     val in = Bits(INPUT, 8)
     val out = Bits(OUTPUT, 8)
-  })
+  }
   val c = Module(new Inner)
   val w = Wire(Bits(NO_DIR, 8))
   w := io.in
