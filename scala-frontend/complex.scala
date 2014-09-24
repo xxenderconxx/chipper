7a8911da9f553d0b98948a5872d61bd07eaecd59
Howard Mao <zhehao.mao@gmail.com>
2014-09-24 11:44:42 -0700

    fix complex example

--- scala-frontend/complex.scala
+++ scala-frontend/complex.scala
@@ -2,24 +2,27 @@ import Chipper._
 import Direction._
 import Builder._
 
-class Complex[T <: Data](val re: T, val im: T) extends Bundle {
+class Complex[T <: Data](val re: T, val im: T, dir: Direction = OUTPUT)
+    extends Bundle(dir) {
   override def cloneType: this.type = {
     println("CLONING COMPLEX")
-    new Complex(re.cloneType, im.cloneType).asInstanceOf[this.type]
+    new Complex(re.cloneType, im.cloneType, dir).asInstanceOf[this.type]
   }
 }
 
 class ComplexAssign(W: Int) extends Module {
   val io = new Bundle {
-    val c  = new Complex(Bits(INPUT, W), Bits(INPUT, W))
-    val re = new Bits(OUTPUT, W)
+    val c  = new Complex(Bits(NO_DIR, W), Bits(NO_DIR, W), INPUT)
+    val d = new Complex(Bits(OUTPUT, W), Bits(OUTPUT, W))
     val e  = new Bool(INPUT)
   }
   when (io.e) {
     val w = Wire(new Complex(Bits(NO_DIR, W), Bits(NO_DIR, W)))
     w := io.c
-    io.re := w.re
+    io.d.re := w.re
+    io.d.im := w.im
   } .otherwise {
-    io.re := Bits(0)
+    io.d.re := Bits(0)
+    io.d.im := Bits(0)
   }
 }
