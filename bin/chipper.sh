ee371fd097f7837b99d93b8ef847911fabd4bdc0
jackbackrack <jrb@pobox.com>
2015-05-20 12:41:30 -0700

    get on-reset working, remove gensyms, fix type + S/UIntLit, add includes

--- bin/chipper.sh
+++ bin/chipper.sh
@@ -1,4 +1,3 @@
 #/bin/bash -f
 
-chipper -i tests/$1.stanza -s generated/$1.s
-gcc -o generated/$1 generated/$1.s /Users/jrb/bar/stanza-375/runtime/runtime.c /Users/jrb/bar/stanza-375/runtime/main.c src/loader.c
+chipper -i tests/$1.stanza -o generated/$1
