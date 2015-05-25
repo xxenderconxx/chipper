4039be1f54c14b25b90d5d438b73bf513986d7f8
jackbackrack <jrb@pobox.com>
2015-05-24 22:17:02 -0700

    get riscv-mini Tile running

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -f
 
 stanza -e src/chipper-syntax.stanza -o chipperc -flags OPTIMIZE
-chipperc -platform os-x -path ~/bar/stanza-382 -install chipper
+chipperc -platform os-x -path ~/bar/stanza-385 -install chipper
