37d6345659e89553967feda171ab9308a93c151d
jackbackrack <jrb@pobox.com>
2015-07-16 00:45:05 -0700

    add flatten macro to defbundle, to-hex, rand, arbiter, fan-out, Vec, source/sinks to tester dpeek, dpoke

--- bin/filter.cpp
+++ bin/filter.cpp
@@ -5,9 +5,16 @@ int main (int argc, char* argv[]) {
   for (;;) {
     c = getchar();
     if (c == EOF) break;
-    if (c == '#' || c == '?')
+    if (c == '#' || c == '?' || c == '@')
       putchar('_');
     else if (c == '$') {
+      //   char nc = getchar();
+      // if (nc >= '0' && nc <= '9') {
+      //   putchar('_'); 
+      // } else {
+      //   putchar(':'); putchar(':');
+      // }
+      // putchar(nc);
       putchar(':'); putchar(':');
     } else
       putchar(c);
